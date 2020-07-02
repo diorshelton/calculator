a112e327d629586fece1cfe6630344621d1f47a1
Dior.Shelton <dior.shelton@gmail.com>
2020-07-01 21:10:23 -0400

    Add README, html and js files to repository

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+
+
+function subtract(number1, number2) {
+  return number1 + number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+
+function mulitiply(number1, number2) {
+  return number1 * number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
